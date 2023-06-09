547111468d596f44dda444e215182b2f828de67e
Cristofe Albuquerque <cristofealbuquerque7@gmail.com>
2023-06-08 23:00:27 -0300

    config-stripe-finished

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,31 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: 'var(--font-roboto)',
+      },
+      colors: {
+        gray: {
+          100: '#E1E1E6',
+          300: '#C4C4CC',
+          800: '#202024',
+          900: '#121214',
+        },
+        green: {
+          300: '#00B37E',
+          500: '#00875F',
+        },
+        gradient: {
+          'top': '#1ea483',
+          'bottom': '#7465d4',
+        },
+      },
+      fontSize: {
+        md: '1.135rem',
+        lg: '1.25rem',
+        xl: '1.5rem',
+        '2xl': '2rem',
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
